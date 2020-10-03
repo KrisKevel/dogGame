eef804b58358201b4103cfaa6d59b10c398541e7
KrisKevel <kevel.kristina@gmail.com>
2020-10-03 14:32:29 +0300

    Basic movement script

---
+++ Assets/Scripts/DogController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogController : MonoBehaviour
+{
+    public float MovementSpeed;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+
+        //Movement
+        Vector2 pos = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            pos.y += MovementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.y -= MovementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            //Change sprite orientation accordingly to movement direction
+            if (!spriteRenderer.flipY)
+            {
+                spriteRenderer.flipY = true;
+            }
+
+            pos.x += MovementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            //Change sprite orientation accordingly to movement direction
+            if (spriteRenderer.flipY)
+            {
+                spriteRenderer.flipY = false;
+            }
+
+            pos.x -= MovementSpeed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+
+        
+    }
+}
