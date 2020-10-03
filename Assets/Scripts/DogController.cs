ea5221c84ce117c93bd78923f54d37b389a21cb5
KrisKevel <kevel.kristina@gmail.com>
2020-10-03 18:49:37 +0300

    Dog is animatedgit add .!

--- Assets/Scripts/DogController.cs
+++ Assets/Scripts/DogController.cs
@@ -8,6 +8,10 @@ public class DogController : MonoBehaviour
     public float MovementSpeed;
     private SpriteRenderer spriteRenderer;
     private Rigidbody2D dogsRigidBody;
+    private Animator animator;
+    private bool movementSideways; 
+    private bool movementAway;
+    private bool movementTowards;
 
     private Vector3 _movementDirection = Vector3.zero;
 
@@ -15,10 +19,14 @@ public class DogController : MonoBehaviour
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         dogsRigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Start()
     {
+        movementSideways = false;
+        movementAway = false;
+        movementTowards = false;
     }
 
     void Update()
@@ -28,14 +36,20 @@ public class DogController : MonoBehaviour
 
         if (Input.GetKey("w"))
         {
+            movementAway = true;
+            animator.SetBool("WalkAway", movementAway);
             moveY = +1f;
         }
         if (Input.GetKey("s"))
         {
+            movementTowards = true;
+            animator.SetBool("WalkTowards", movementTowards);
             moveY = -1f;
         }
         if (Input.GetKey("d"))
         {
+            movementSideways = true;
+            animator.SetBool("WalkSide", movementSideways);
             //Change sprite orientation accordingly to movement direction
             if (!spriteRenderer.flipX)
             {
@@ -46,6 +60,8 @@ public class DogController : MonoBehaviour
         }
         if (Input.GetKey("a"))
         {
+            movementSideways = true;
+            animator.SetBool("WalkSide", movementSideways);
             //Change sprite orientation accordingly to movement direction
             if (spriteRenderer.flipX)
             {
@@ -54,6 +70,16 @@ public class DogController : MonoBehaviour
 
             moveX = -1f;
         }
+
+        if (!movementSideways)animator.SetBool("WalkSide", movementSideways);
+        else movementSideways = false;
+
+        if (!movementAway) animator.SetBool("WalkAway", movementAway);
+        else movementAway = false;
+
+        if (!movementTowards) animator.SetBool("WalkTowards", movementTowards);
+        else movementTowards = false;
+
 
         _movementDirection = new Vector3(moveX, moveY).normalized;
     }
