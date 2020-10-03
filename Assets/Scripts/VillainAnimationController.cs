d875769029e02a3f0daacdf61b34b684288117d6
Miras Kenzhegaliyev <kenzhemir@gmail.com>
2020-10-03 22:19:22 +0300

    Add villian movements

---
+++ Assets/Scripts/VillainAnimationController.cs
@@ -0,0 +1,27 @@
+﻿using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillainAnimationController : MonoBehaviour
+{
+    AIPath aipath;
+    Animator animator;
+
+    private void Awake()
+    {
+        aipath = GetComponentInParent<AIPath>();
+        animator = GetComponent<Animator>();
+    }
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Horizontal", aipath.desiredVelocity.x);
+        animator.SetFloat("Vertical", aipath.desiredVelocity.y);
+        animator.SetFloat("Speed", aipath.desiredVelocity.magnitude);
+    }
+}
--- Assets/Scripts/VillianBehaviour.cs
+++ Assets/Scripts/VillianBehaviour.cs
@@ -1,15 +1,41 @@
-﻿using System.Collections;
+﻿using Pathfinding;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class VillianBehaviour : MonoBehaviour
 {
-    
 
+    private Vector3 _destination;
+    public Vector3 destination
+    {
+        get
+        {
+            return _destination;
+        }
+        set
+        {
+            _destination = value;
+            MoveToDestination();
+        }
+    }
+
+    private AIPath aipath;
+
+    private void MoveToDestination()
+    {
+        aipath.destination = _destination;   
+    }
+
+    private void Awake()
+    {
+        aipath = GetComponent<AIPath>();
+    }
 
     void Start()
     {
-        
+        destination = Vector3.zero;
     }
 
     // Update is called once per frame
