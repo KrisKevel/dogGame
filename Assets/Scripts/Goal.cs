45cb261cc32706d23e43ef78c4559133e575c594
KrisKevel <kevel.kristina@gmail.com>
2020-10-03 22:15:02 +0300

    Objective setup

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Goals of the player listed
+
+public abstract class Goal : MonoBehaviour
+{
+    public string name;
+    public abstract bool IsAchieved();
+    public abstract void Complete();
+    public abstract void DrawHUD();
+}
+
+public class GoalManager : MonoBehaviour
+{
+
+    public Goal[] goals;
+
+    void Awake()
+    {
+        goals = GetComponents<Goal>();
+    }
+
+    void OnGUI()
+    {
+        //UIController.SetObjectives(goals);
+    }
+
+    void Update()
+    {
+        foreach (var goal in goals)
+        {
+            if (goal.IsAchieved())
+            {
+                goal.Complete();
+                Destroy(goal);
+            }
+        }
+    }
+}
+
+class Eat : Goal
+{
+    public override bool IsAchieved()
+    {
+        return false;
+    }
+
+    public override void Complete()
+    {
+        
+    }
+
+    public override void DrawHUD()
+    {
+        
+    }
+}
+
+class Nap : Goal
+{
+    public override bool IsAchieved()
+    {
+        return false;
+    }
+
+    public override void Complete()
+    {
+        
+    }
+
+    public override void DrawHUD()
+    {
+        
+    }
+}
+
+class Play : Goal
+{
+    public override bool IsAchieved()
+    {
+        return false;
+    }
+
+    public override void Complete()
+    {
+        
+    }
+
+    public override void DrawHUD()
+    {
+        
+    }
+}
+
+class Bark : Goal
+{
+    public override bool IsAchieved()
+    {
+        return false;
+    }
+
+    public override void Complete()
+    {
+        
+    }
+
+    public override void DrawHUD()
+    {
+        
+    }
+}
+
+class ChaseTail : Goal
+{
+    public override bool IsAchieved()
+    {
+        return false;
+    }
+
+    public override void Complete()
+    {
+        
+    }
+
+    public override void DrawHUD()
+    {
+        
+    }
+}
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,18 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UIController : MonoBehaviour
 {
+    public static UIController Instance;
+
+    public TextMeshProUGUI GoalPrefab;
+    public GameObject ObjectivePanel;
+    //public Image SuspicionMeter;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+    }
+
+    public void SetSuspicious(float sus)
+    {
+        //Set a value to the susect meter
+    }
+
+    public void SetObjectives(Goal[] goals)
+    {
+        foreach (Goal goal in goals) {
+            GoalPrefab.text = goal.name;
+            GameObject.Instantiate(GoalPrefab, ObjectivePanel.transform);
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
 }
