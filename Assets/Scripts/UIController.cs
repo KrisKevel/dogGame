cc1b6f79f42234700b476705ef19d10659eb3367
KrisKevel <kevel.kristina@gmail.com>
2020-10-04 13:12:54 +0300

    New sprites

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -11,19 +11,38 @@ public class UIController : MonoBehaviour
 
     public TextMeshProUGUI GoalPrefab;
     public GameObject ObjectivePanel;
+    public GameObject NarrativePanel;
     public Image SuspicionMeter;
+    public TextMeshProUGUI Narrative;
+    public float TimeToRead = 15;
 
     public Dictionary<string, TextMeshProUGUI> quests;
+    public Dictionary<int, string> narrative;
+
+    private float timeToRead;
+    private int currentNarr;
 
     private void Awake()
     {
         quests = new Dictionary<string, TextMeshProUGUI>();
         Instance = this;
+        
+        narrative = new Dictionary<int, string>();
+        narrative.Add(0, "Being a dog isn't easy, is it? Day after day its all the same - ");
+        narrative.Add(1, "eat, sleep, eventually chase own tail and wait for the owner to come back from work.");
+        narrative.Add(2, "(what are they even doing there? weird fellas)");
+        narrative.Add(3, "But you know, some dogs are really, really different. You don't believe me?");
+        narrative.Add(4, "Well, let me introduce you to Rex.");
+        narrative.Add(5, "Now don't get me wrong, he still absolutely has to do 'the dog things'");
+        narrative.Add(5, "but there is something bigger, something more important, a mission!");
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        currentNarr = -1;
+        timeToRead = 0;
+
     }
 
     public void SetSuspicious(float sus)
@@ -69,5 +88,22 @@ public class UIController : MonoBehaviour
         {
             Application.Quit();
         }
+
+        if(timeToRead <= 0)
+        {
+            if (NarrativePanel.transform.childCount > 0)
+            {
+                Debug.Log("STORY CHANGE");
+                Destroy(NarrativePanel.transform.GetChild(0).gameObject);
+            }
+            
+            narrative.TryGetValue(++currentNarr, out string nar);
+            Narrative.text = nar;
+            GameObject.Instantiate(Narrative, NarrativePanel.transform);
+            timeToRead = TimeToRead;
+
+        }
+
+        timeToRead -= Time.deltaTime;
     }
 }
