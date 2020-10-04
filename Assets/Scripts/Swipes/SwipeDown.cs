29d0859d902a64e1c477f2b26a735637ed71d6f1
Miras Kenzhegaliyev <kenzhemir@gmail.com>
2020-10-04 14:12:26 +0300

    Add swipes

---
+++ Assets/Scripts/Swipes/SwipeDown.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeDown : MonoBehaviour
+{
+    public float SwipeRange;
+
+    private Vector3 startPos;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPos = Input.mousePosition;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            if ( - (Input.mousePosition - startPos).y > SwipeRange)
+            {
+                Swipe();
+            }
+        }
+    }
+
+    private void Swipe()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Swipes/SwipeUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeUp : MonoBehaviour
+{
+    public float SwipeRange;
+
+    private Vector3 startPos;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPos = Input.mousePosition;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            if ((Input.mousePosition - startPos).y > SwipeRange)
+            {
+                Swipe();
+            }
+        }
+    }
+
+    private void Swipe()
+    {
+        gameObject.SetActive(false);
+    }
+}
